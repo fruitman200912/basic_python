dd00feadbb7b1092e08ec26e8b07f692881819c9
fruitman <fruitman@fruitmanui-MacBookAir.local>
2025-03-13 08:25:16 +0900

    commit_3

--- basic.py
+++ basic.py
@@ -1,3 +1,8 @@
-print("\\\"Hello\\\" , \\\'GitHub\\\'")
-print("Hello")
-print("GitHub")
+c1, c2 = input("c1 , c2:").split()
+print(c1[0:2] + c2[1] * 3)
+
+n1, n2 = input("n1:n2:").split(':')
+print(int(n2) , int(n1), sep=':')
+
+f1, f2 = input("f:").split()
+print(float(f1), float(f2), sep='')
