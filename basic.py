422f447c8fa706484088bf559f2522bbd1454b7e
fruitman <fruitman@fruitmanui-MacBookAir.local>
2025-03-13 08:29:53 +0900

    commit_3.1

--- basic.py
+++ basic.py
@@ -1,8 +1,8 @@
-c1, c2 = input("c1 , c2:").split()
-print(c1[0:2] + c2[1] * 3)
+s1, s2 = input("s1 , s2:").split()
+print(s1[0:2] + s2[1] * 3)
 
 n1, n2 = input("n1:n2:").split(':')
 print(int(n2) , int(n1), sep=':')
 
 f1, f2 = input("f:").split()
-print(float(f1), float(f2), sep='')
+print('\\\"',float(f1), float(f2),'\\\'', sep='')
